615e958cde4af6020612f9441bac0834569b2291
조우현 <darkrubyeye@gmail.com>
2015-12-07 01:01:22 +0900

    패키지 명 변경

--- src/main/java/com/tmon/sb/web/DemoController.java
+++ src/main/java/com/tmon/sb/web/DemoController.java
@@ -1,4 +1,4 @@
-package com.tmon.sb.contoller;
+package com.tmon.sb.web;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
